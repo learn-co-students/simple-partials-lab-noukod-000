cb1a6a14a3483b3b5de61f0cba4a3972eebceeec
Certil Remy <certil@noukod.com>
2019-01-28 16:49:57 +0000

    Done.

--- spec/views/classroom_student_spec.rb
+++ spec/views/classroom_student_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe "classroom_show_view" do
+describe "classroom_show_view" do
   it "renders classroom information on the show view" do
     view.lookup_context.prefixes = %w[students]
     student = Student.create(name: 'Bobby', hometown: Faker::Address.city, birthday: Faker::Date.between(25.years.ago, 18.years.ago))
